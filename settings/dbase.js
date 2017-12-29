0dca6f309e32dc24b51c570fd41a7b74f1320f86
prince sackey <prinsac23@gmail.com>
2017-12-29 02:00:54 +0100

    Database configuration file(Database settings)

---
+++ settings/dbase.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto').randomBytes(256).toString('hex');
+
+module.exports = {
+    uri: 'mongodb://localhost:27017/' + this.db,
+    secret : crypto,
+    db : 'vklinik',
+
+}
