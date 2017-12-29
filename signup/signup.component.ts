9d12b32fea2c11ebb886d4b6b40f2e49e21aa186
uchedibie <uche.dibie@yahoo.com>
2017-12-29 17:11:43 +0100

    Signup components

---
+++ signup/signup.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RegauthService } from '../../services/regauth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class SignupComponent implements OnInit {
+  form: FormGroup;
+  message;
+  messageClass;
+
+  constructor(private formBuilder: FormBuilder, private regauthService: RegauthService) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      username: '',
+      email: '',
+      password: '',
+      confirm_password: ''
+    });
+  }
+
+  // this block of code captures and submits form 
+  onSignup() {
+    const user = {
+      username: this.form.get('username').value,
+      email: this.form.get('email').value,
+      password: this.form.get('password').value,
+  };
+    this.regauthService.signupUser(user).subscribe(data => {
+      console.log(data);
+
+      if (data.sucess) {
+        this.messageClass = 'alert alert-danger';
+        this.message = data.message;
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
