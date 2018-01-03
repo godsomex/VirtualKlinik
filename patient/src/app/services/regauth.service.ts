a85712f64ed7e96d0aa4854032aa3b5a1ccd8d02
Kwabena Obeng Joehene <Kwabena.Joehene@boaghana.com>
2018-01-03 13:21:03 +0100

    frontend login, signup and browser token services

--- patient/src/app/services/regauth.service.ts
+++ patient/src/app/services/regauth.service.ts
@@ -7,6 +7,8 @@ import 'rxjs/add/operator/map';
 export class RegauthService {
 
 domain = 'http://localhost:9000';
+authenticationToken;
+user;
 
   constructor(
     private http: Http
@@ -15,4 +17,13 @@ domain = 'http://localhost:9000';
 signupUser(user) {
   return this.http.post(this.domain + '/registration/register', user).map(res => res.json());
 }
+  login(user) {
+    return this.http.post(this.domain + '/registration/login', user).map(res => res.json());
+  }
+  browserUserData(token, user) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify('user'));
+    this.authenticationToken = token;
+    this.user = user;
+  }
 }
